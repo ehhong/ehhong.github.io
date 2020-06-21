4cc7125d188eec72b15abccf4d32ac480b8ec018
ehhhmily <31592018+ehhhmily@users.noreply.github.com>
2020-06-20 17:54:06 -0700

    enabled ability to create photo series in art + design page(s)

--- _includes/get-image.js
+++ _includes/get-image.js
@@ -1,24 +1,38 @@
 // this script will automatically get image from URL
-var getImage = function () {
+var getImages = function () {
     var queryString = document.location.search.split("+").join(" ");
     var params = {}, tokens, re = /[?&]?([^=]+)=([^&]*)/g;
 
     while (tokens = re.exec(queryString)) {
         params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
     }
+    var file_str = params.img;
 
-    if (params.img == undefined) {
+    if (file_str == undefined) {
         // no value in the query string
         // here you have to manage this use case : redirect or print a message to user
     } else {
         var image = document.getElementById('image');
-        image.src = "assets/art/" + params.img;
-        image.alt = params.img;
+        image.src = "assets/art/" + file_str;
+        image.alt = file_str;
 
         var title = document.getElementById('page-title');
-        var image_title = getTitleFromFileName(params.img);
+        var image_title = getTitleFromFileName(file_str);
         title.innerHTML = image_title.italics();
         document.title = document.title.replace("image", image_title);
+
+        // display serial images if appropriate
+        var ctr = 1;
+        var period_idx = file_str.indexOf(".");
+        var serial_file_str = file_str.substring(0, period_idx) + "_" + ctr + file_str.substring(period_idx, file_str.length);
+
+        while (doesFileExist("assets/art/" + serial_file_str)) {
+            var serial_img = document.createElement('img');
+            serial_img.src =  "assets/art/" + serial_file_str;
+            document.getElementsByClassName('content')[0].appendChild(serial_img);
+
+            serial_file_str = serial_file_str.replace(ctr, ++ctr);
+        }
     }
 } ();
 
@@ -27,4 +41,16 @@ function getTitleFromFileName(fileName) {
     fileName = fileName.replace(".jpg", "");
     fileName = fileName.replace(/-/g, " ");
     return fileName;
+}
+
+function doesFileExist(urlToFile) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('HEAD', urlToFile, false);
+    xhr.send();
+
+    if (xhr.status == "404") {
+        return false;
+    } else {
+        return true;
+    }
 }
--- image.md
+++ image.md
@@ -4,7 +4,20 @@ title: image
 sidebar_link: false
 ---
 
-<img id="image" src="nametag.png" alt="placeholder">
+<!--
+    MAIN IMAGES:
+        image titles are displayed with the following file naming convention: "file-name.png" -> "file name"
+    SERIAL IMAGES:
+        to avoid displaying images series on main art page,
+        serial images must be named with the following convention:
+            - file-name.png
+            - file-name_1.png
+            - file-name_2.png
+            - file-name_3.png
+            ...
+-->
+
+<img id="image" src="" alt="placeholder">
 
 <script>
     {% include get-image.js %}
