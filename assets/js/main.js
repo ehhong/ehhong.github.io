62c71fdeb71e8ce86eb8135633fbc434418f3df6
ehhhmily <31592018+ehhhmily@users.noreply.github.com>
2021-03-23 11:41:22 -0700

    added tablet responsive image sizes

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,8 @@ $(window).on("load", function(){
     var lamp_height = $("img#lamp").height();
     var window_height = $(window).height();
 
-    // non-mobile
-    if ($(window).width() > 600) {
+    // desktop
+    if ($(window).width() > 1024) {
         var dscale_ratio = (0.70 * window_height) / lamp_height // ratio based off of lamp height
 
         $('img.home').each(function() {
@@ -19,8 +19,21 @@ $(window).on("load", function(){
             $(this).show()
         });
     }
+    // vertical tablet
+    else if ($(window).width() > 600) {
+        var dscale_ratio = (0.45 * window_height) / lamp_height // ratio based off of lamp height
+        $('img.home').each(function() {
+            // scale images down
+            var og_height = $(this).height(),
+            new_height = Math.round(og_height * dscale_ratio);
+            $(this).height(new_height);
+
+            $(this).show();
+        });
+
+    }
     // mobile
-    else if ($(window).width() < 600) {
+    else if ($(window).width() <= 600) {
         var dscale_ratio = (0.5 * window_height) / lamp_height // ratio based off of lamp height
 
         $('img.home').each(function() {
